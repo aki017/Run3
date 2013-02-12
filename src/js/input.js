d9210cd499f7e8e6b72e1cfd08080c5a7fc4a112
bitterfox <bitterfox@example.com>
2013-02-12 21:10:29 +0900

    Impl InputManager: string & number base -> number base
    Indent InputManager: Indent KEY_XXX
    
    Fix game.js: InputManagerの変更に追随

--- src/js/input.js
+++ src/js/input.js
@@ -1,14 +1,25 @@
+
+var KEY_BS    = 8;
+var KEY_ENTER = 13;
+var KEY_SHIFT = 16;
+var KEY_CTRL  = 17;
+var KEY_ALT   = 18;
+var KEY_SPACE = 32;
+var KEY_LEFT  = 37;
+var KEY_UP    = 38;
+var KEY_RIGHT = 39;
+var KEY_DOWN  = 40;
+
 InputManager = function(){
     var self       = this;
     var nowInput   = {};
     var bitNumbers = [];
 
-    var keyNames   = "KEY_BS KEY_ENTER KEY_SHIFT KEY_CTRL KEY_ALT KEY_SPACE KEY_LEFT KEY_UP KEY_RIGHT KEY_DOWN".split(/ /);
-    var keyNumbers = [8,     13,       16,       17,      18,     32,       37,      38,    39,       40];
+    var keyNumbers   = [KEY_BS, KEY_ENTER, KEY_SHIFT, KEY_CTRL, KEY_ALT, KEY_SPACE, KEY_LEFT, KEY_UP, KEY_RIGHT, KEY_DOWN];
 
-    for (var i in keyNames)
+    for (var i in keyNumbers)
     {
-        nowInput[keyNames[i]]     = 0;
+        nowInput[keyNumbers[i]]   = 0;
         bitNumbers[keyNumbers[i]] = i
     }
 
@@ -23,14 +34,9 @@ InputManager = function(){
     this.getframes = function(type){
         if (type === null) 
             throw new Error("type must not be null");
-        if (typeof type == 'undefined') 
-            throw new Error("type must not be undefined");
-        if (typeof type == 'boolean') 
-            throw new Error("type must not be boolean");
-        if (typeof type == 'number') 
-            throw new Error("Not Implemented");
+        if (typeof type != 'number')
+            throw new Error("type must be number");
 
-        if (typeof type == 'string') 
             return nowInput[type];
     }
     this.ispushed = function(type){
@@ -41,12 +47,11 @@ InputManager = function(){
         {
             if(inputbit & 1 << bitNumbers[keyNumbers[i]])
             {
-                nowInput[keyNames[i]]++;
+                nowInput[keyNumbers[i]]++;
             }else{
-                nowInput[keyNames[i]] = 0;
+                nowInput[keyNumbers[i]] = 0;
             }
         }
-        
     }
     $(window).on("keydown", function(e){ self.keydown(e.keyCode)});
     $(window).on("keyup",   function(e){ self.keyup(e.keyCode)  });
