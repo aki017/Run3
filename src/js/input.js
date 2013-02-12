05b834eab27fe55204e62d77f803c96b3deaee1b
aki017 <aki@aki017.info>
2013-02-12 18:09:10 +0900

    Optimize InputManager
    未使用変数の削除
    remove InputManager#getkey
    add    InputManager#getframes
    add    InputManager#ispushed

--- src/js/input.js
+++ src/js/input.js
@@ -1,37 +1,53 @@
 InputManager = function(){
-    var keyCodePair = {};
-    var nowInput    = {};
-    var bitNumber   = [];
+    var self       = this;
+    var nowInput   = {};
+    var bitNumbers = [];
 
-    var keyNames    = "KEY_BS KEY_ENTER KEY_SHIFT KEY_CTRL KEY_ALT KEY_SPACE KEY_LEFT KEY_UP KEY_RIGHT KEY_DOWN".split(/ /);
-    var keyNumber   = [8, 13, 16, 17, 18, 32, 37, 38, 39, 40];
+    var keyNames   = "KEY_BS KEY_ENTER KEY_SHIFT KEY_CTRL KEY_ALT KEY_SPACE KEY_LEFT KEY_UP KEY_RIGHT KEY_DOWN".split(/ /);
+    var keyNumbers = [8,     13,       16,       17,      18,     32,       37,      38,    39,       40];
 
     for (var i in keyNames)
     {
-        keyCodePair[keyNames[i]] = keyNumber[i];
-        nowInput[keyNames[i]]    = 0;
-        bitNumber[keyNumber[i]]  = i
+        nowInput[keyNames[i]]     = 0;
+        bitNumbers[keyNumbers[i]] = i
     }
 
     var inputbit = 0;
+
     this.keydown = function(i){
-        inputbit |= 1 << bitNumber[i];
+        inputbit |= 1 << bitNumbers[i];
     }
-    this.keyup   = function(i){
-        inputbit &= ~(1 << bitNumber[i]);
+    this.keyup = function(i){
+        inputbit &= ~(1 << bitNumbers[i]);
     }
-    this.getkey  = function(){ return nowInput; }
+    this.getframes = function(type){
+        if (type === null) 
+            throw new Error("type must not be null");
+        if (typeof type == 'undefined') 
+            throw new Error("type must not be undefined");
+        if (typeof type == 'boolean') 
+            throw new Error("type must not be boolean");
+        if (typeof type == 'number') 
+            throw new Error("Not Implemented");
+
+        if (typeof type == 'string') 
+            return nowInput[type];
+    }
+    this.ispushed = function(type){
+        return this.getframes(type) > 0;
+    }
     this.update  = function(){
-        for(var i = 0; i < keyNumber.length ; i++ )
+        for(var i = 0; i < keyNumbers.length ; i++ )
         {
-            if(inputbit & 1 << bitNumber[keyNumber[i]])
+            if(inputbit & 1 << bitNumbers[keyNumbers[i]])
             {
-                nowInput[keyNumber[i]]++;
+                nowInput[keyNames[i]]++;
             }else{
-                nowInput[keyNumber[i]]=0;
+                nowInput[keyNames[i]] = 0;
             }
         }
+        
     }
-    $(window).on("keydown", function(e){input.keydown(e.keyCode)});
-    $(window).on("keyup", function(e){input.keyup(e.keyCode)});
+    $(window).on("keydown", function(e){ self.keydown(e.keyCode)});
+    $(window).on("keyup",   function(e){ self.keyup(e.keyCode)  });
 }
